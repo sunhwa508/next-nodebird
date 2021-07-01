e6579a8a5d256ecb76df85206422b8f9f4844b5f
sunhwa508 <2017sunhwa@gmail.com>
2021-07-01 22:49:14 +0900

    add eslint

--- front/components/AppLayout.tsx
+++ front/components/AppLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import Link from 'next/link'
 interface Props {
     children: React.ReactNode;
   }
@@ -7,7 +7,11 @@ interface Props {
 const AppLayout = ({children}:Props) => {
     return (
         <div>
-            <div>공통메뉴</div>
+            <div>
+                <Link href="/"><a>노드버드</a></Link>
+                <Link href="/profile"><a>프로필</a></Link>
+                <Link href="/signup"><a>회원가입</a></Link>
+            </div>
             {children}
         </div>
     )
