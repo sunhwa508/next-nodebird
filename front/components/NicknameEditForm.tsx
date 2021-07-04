f5d3a348107a9d1e4ae2e72c9c4befc51a5e2c22
sunhwa508 <2017sunhwa@gmail.com>
2021-07-04 19:02:38 +0900

    update profile pages

---
+++ front/components/NicknameEditForm.tsx
@@ -0,0 +1,17 @@
+import { useMemo } from 'react'
+import { Form, Input } from 'antd';
+
+const NicknameEditForm = () => {
+    const style = useMemo(() => ({
+        marginBottom: '20px', border: "1px solid #d9d9d9", padding: '20px'
+    }), [])
+
+    // react form 라이브러리 사용 권장
+    return (
+        <Form style={style}>
+            <Input.Search addonBefore="닉네임" enterButton="수정" />
+        </Form>
+    )
+}
+
+export default NicknameEditForm
