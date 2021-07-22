67d3da9f540316ddc0c978ea01b48078048176e1
sunhwa <2017sunhwa@gmail.com>
2021-07-22 12:27:08 +0900

    🐛 useRef type bug 수정

--- front/components/PostForm.tsx
+++ front/components/PostForm.tsx
@@ -1,56 +1,56 @@
+import React, { useRef, useCallback, useState, useEffect } from 'react';
 import { Form, Input, Button } from 'antd';
-import React, { useState, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import styled from 'styled-components';
-import { rootType } from '../reducers';
-import { addPost } from '../reducers/post'
 
-const StyledForm = styled(Form)`
-    margin: '10px 0 20px';
-`
-const StyledButton = styled(Button)`
-    float: 'right';
-`;
+import { addPost } from '../reducers/post';
+import { rootType } from '../reducers';
 
 const PostForm = () => {
-    const { imagePaths } = useSelector((state: rootType) => state.post);
-    // Ref 실제 dom 에 접근하기 위해 사용된다.
-    const imageInput = useRef()
+    const { imagePaths, postAdded } = useSelector((state: rootType) => state.post);
+    const [text, setText] = useState('');
+    const dispatch = useDispatch();
+    const imageInput = useRef<HTMLInputElement>(null);
 
     const onClickImageUpload = useCallback(() => {
-        imageInput.current.click()
-    }, [imageInput.current])
+        imageInput.current?.click();
+    }, [imageInput.current]);
 
-    const [text, setText] = useState('');
-    const onChangeText = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setText(e.target.value)
-    }, [])
+    useEffect(() => {
+        if (postAdded) {
+            setText('');
+        }
+    }, [postAdded]);
 
-    const dispatch = useDispatch();
+    const onChangeText = useCallback((e) => {
+        setText(e.target.value);
+    }, []);
+
     const onSubmit = useCallback(() => {
-        dispatch(addPost)
-        setText('')
-    }, [])
+        dispatch(addPost);
+    }, []);
 
     return (
-        <StyledForm encType="multipart/form-data" onFinish={onSubmit}>
-            <Input.TextArea value={text} onChange={(e) => onChangeText(e)} maxLength={140} placeholder="어떤 일이 있었나요~?" />
+        <Form style={{ margin: '10px 0 20px' }} encType="multipart/form-data" onFinish={onSubmit}>
+            <Input.TextArea value={text} onChange={onChangeText} maxLength={140} placeholder="어떤 신기한 일이 있었나요?" />
             <div>
                 <input type="file" multiple hidden ref={imageInput} />
-                <StyledButton onClick={onClickImageUpload}>이미지 업로드</StyledButton>
-                <StyledButton type="primary" htmlType="submit">짹짹</StyledButton>
+                <Button onClick={onClickImageUpload}>이미지 업로드</Button>
+                <Button type="primary" style={{ float: 'right' }} htmlType="submit">짹짹</Button>
             </div>
             <div>
-                {imagePaths.map((v: any) => (
-                    <div key={v} style={{ display: 'inline-block' }}>
-                        <img src={v} style={{ width: '200px' }} alt={v} />
-                        <div>
-                            <button>제거</button>
+                {imagePaths.map((v) => {
+                    return (
+                        <div key={v} style={{ display: 'inline-block' }}>
+                            <img src={'http://localhost:3065/' + v} style={{ width: '200px' }} alt={v} />
+                            <div>
+                                <Button>제거</Button>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    )
+                })}
             </div>
-        </StyledForm>
-    )
-}
-export default PostForm
+        </Form>
+    );
+};
+
+export default PostForm;
