51910735b89965bf486fff4a566178ad8b4458bf
sunhwa <2017sunhwa@gmail.com>
2021-09-22 14:58:22 +0900

    ✨[feat] 미들웨어 추가

--- back/app.js
+++ back/app.js
@@ -1,57 +1,65 @@
 const express = require("express");
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
-const passport = require('passport')
+const passport = require("passport");
 const postRouter = require("./routes/post");
 const userRouter = require("./routes/user");
-const db = require('./models');
-const passportConfig = require('./passport');
-const dotenv = require('dotenv')
+const db = require("./models");
+const passportConfig = require("./passport");
+const dotenv = require("dotenv");
 
 dotenv.config();
 
 const app = express();
-db.sequelize.sync()
-  .then(()=>{
-    console.log('db 연결 성공');
+db.sequelize
+  .sync()
+  .then(() => {
+    console.log("db 연결 성공");
   })
   .catch(console.error);
 passportConfig();
 
-app.use(cors({
-  origin: '*',
-  credentials: false,
-}));
+app.use(
+  cors({
+    origin: "*",
+    credentials: false,
+  }),
+);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser(process.env.COOKIE_SECRET));
-app.use(session({
-  saveUninitialized:false,
-  resave:false,
-  secret: process.env.COOKIE_SECRET,
-}));
+app.use(
+  session({
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.COOKIE_SECRET,
+  }),
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.get("/api", (req, res) => {
-  res.send('hello')
+  res.send("hello");
 });
 
 app.get("/api/posts", (req, res) => {
   res.json([
-    {id:1, content:'hello'},
-    {id:2, content:'hello2'},
-    {id:3, content:'hello3'},
+    { id: 1, content: "hello" },
+    { id: 2, content: "hello2" },
+    { id: 3, content: "hello3" },
   ]);
 });
 
-app.use('/post', postRouter);
-app.use('/user', userRouter);
+app.use("/post", postRouter);
+app.use("/user", userRouter);
 
+// 에러던지면 여기로 떨어지며 내부적으로 존재함
+// 직접 정의도 가능함
+app.use((err, req, res, next) => {});
 
 app.listen(3065, () => {
   console.log("서버 실행 중");
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,73 +1,80 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const passport = require('passport');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 
-const { User, Post } = require('../models');
+const { User, Post } = require("../models");
+const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
 const router = express.Router();
 
-router.post('/login', (req, res, next)=>{ //POST /user/login
-    passport.authenticate('local', (err, user, info)=>{
-        if(err){
-            console.errer(err);
-            next(err); //express가 에러를 처리할 수 있게 보내버린다.
-        }
-        if(info){
-            return res.status(401).send(info.reason);
-        }
-        return req.login(user, async(loginErr) => {
-            if(loginErr){
-                console.error(loginErr)
-                return next(loginErr);
-            }
-            const fullUserWithoutPassword = await User.findOne({
-                where: {id: user.id},
-                attributes: {
-                    exclude: ['password']
-                },
-                include:[{
-                    model: Post,
-                },{
-                    model: User,
-                    as: 'Followings',
-                },{
-                    model: User,
-                    as: 'Followers',
-                }]
-            });
-            return res.status(200).json(fullUserWithoutPassword);
-        });
-    })(req,res,next);
-})
-
-router.post('/', async (req, res, next)=>{ //POST /user/
-    try{
-        //이메일 중복체크
-        const exUser = await User.findOne({
-            where:{
-                email: req.body.email,
-            }
-        })
-        if(exUser){
-            return res.status(403).send('이미 사용중인 아이디입니다.');
-        }
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        await User.create({
-            email: req.body.email,
-            nickname:req.body.nickname,
-            password: hashedPassword,
-        });
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.status(200).send('ok');
-    }catch(error){
-        console.error(error);
-        next(error) //status 500
+router.post("/login", isNotLoggedIn, (req, res, next) => {
+  //POST /user/login
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      console.errer(err);
+      next(err); //express가 에러를 처리할 수 있게 보내버린다.
     }
+    if (info) {
+      return res.status(401).send(info.reason);
+    }
+    return req.login(user, async loginErr => {
+      if (loginErr) {
+        console.error(loginErr);
+        return next(loginErr);
+      }
+      const fullUserWithoutPassword = await User.findOne({
+        where: { id: user.id },
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: Post,
+          },
+          {
+            model: User,
+            as: "Followings",
+          },
+          {
+            model: User,
+            as: "Followers",
+          },
+        ],
+      });
+      return res.status(200).json(fullUserWithoutPassword);
+    });
+  })(req, res, next);
 });
 
-router.post('/logout', (req, res, next)=>{
-    req.logout();
-    req.session.destroy();
-    res.send('ok');
+router.post("/", isNotLoggedIn, async (req, res, next) => {
+  //POST /user/
+  try {
+    //이메일 중복체크
+    const exUser = await User.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    if (exUser) {
+      return res.status(403).send("이미 사용중인 아이디입니다.");
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.status(200).send("ok");
+  } catch (error) {
+    console.error(error);
+    next(error); //status 500
+  }
+});
+
+router.post("/logout", isLoggedIn, (req, res, next) => {
+  req.logout();
+  req.session.destroy();
+  res.send("ok");
 });
 
 module.exports = router;
